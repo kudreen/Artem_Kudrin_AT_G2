0cc6bf77d8aad3b8ac4866bd8f84c487a2730897
Artem Kudrin <kudreen@gmail.com>
2021-02-16 22:37:19 +0300

    -

---
+++ src/main/java/tasks/day1/src/Calc.java
@@ -0,0 +1,15 @@
+public class Calc {
+
+    public int add(int x, int y) {
+        return x + y;
+    }
+
+    public int mult(int x, int y) {
+        return x * y;
+    }
+
+    public int divide(int x, int y) {
+        return x / y;
+    }
+
+}
