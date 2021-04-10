5929355e1700cb99b924768fa0f9cd42c8f39c23
Artem Kudrin <kudreen@gmail.com>
2021-04-10 14:12:18 +0300

    add tests to BottleTest

--- src/main/java/project/test/BottleTest.java
+++ src/main/java/project/test/BottleTest.java
@@ -1,10 +1,9 @@
 package test;
 
-import com.company.vessel.Bottle;
+import vessel.Bottle;
 import org.junit.Test;
 
-import static com.company.material.Material.Metal;
-import static com.company.material.Material.Plastic;
+import static material.Material.Plastic;
 import static org.junit.Assert.assertEquals;
 
 public class BottleTest{
@@ -16,9 +15,17 @@ public class BottleTest{
     public void materialTest() {
        Bottle bottle = new Bottle(1, 2.0, Plastic);
 
-        //assertEquals("Bottle volume is not as expected!", bottle.getVolume(), 1);
-        assertEquals(1, 1, 0);
-        //assertEquals("Bottle diameter is not as expected!", bottle.getDiameter(), 2.0);
+        assertEquals(2.0, bottle.getDiameter(), 0);
         assertEquals("Bottle material is not as expected!", bottle.getMaterial(), Plastic);
+    }
+    @Test
+    public void volumeTest() {
+        Bottle bottle = new Bottle(1, 2.0, Plastic);
+        assertEquals(0.5, bottle.getVolume(), 0);
+    }
+    @Test
+    public void diameterTest() {
+        Bottle bottle = new Bottle(1, 2.0, Plastic);
+        assertEquals(2.0, bottle.getDiameter(), 0);
     }
 }
