8c7dab805abe99c7b6a0e129b13ec6bccf353070
Artem Kudrin <kudreen@gmail.com>
2021-05-01 02:59:04 +0300

    new test packages

--- src/test/java/steps/BaseSteps.java
+++ src/test/java/steps/BaseSteps.java
@@ -1,7 +1,9 @@
 package steps;
 
 
+import day19.L4JLogging;
 import driver.Driver;
+import org.apache.log4j.Logger;
 import org.junit.AfterClass;
 import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
@@ -9,27 +11,45 @@ import org.openqa.selenium.WebDriver;
 import java.util.concurrent.TimeUnit;
 
 public class BaseSteps {
-    public static WebDriver driver;
+   // public static WebDriver driver;
+    static WebDriver driver = Driver.getWebDriver();
+    private static final Logger LOGGER = Logger.getLogger(L4JLogging.class.getName());
 
     @BeforeClass
     public static void initDriver() {
+        LOGGER.info("Start of test");
         driver = Driver.getWebDriver();
-        //driver = new Driver();
+    }
+
+    @AfterClass
+    public static void closeDriver() {
+        LOGGER.info("End of test");
+        Driver.getWebDriver().close();
+    }
+
+
+    public static void openPageAdress(String pagename){
+        LOGGER.debug("Step openPageAdress");
+        driver.get(pagename);
     }
 
     public static void ppageLoadTimeout() {
+        LOGGER.debug("Timeout ppageLoadTimeout");
         driver = Driver.getWebDriver();
         driver.manage().timeouts().pageLoadTimeout(20, TimeUnit.SECONDS);
     }
 
+    public static void implicitlyWait() {
+        LOGGER.debug("Timeout implicitlyWait");
+        driver.manage().timeouts().implicitlyWait(5, TimeUnit.SECONDS);
+    }
+
     public static void scriptLoadTimeout() {
+        LOGGER.debug("Timeout scriptLoadTimeout");
         driver = Driver.getWebDriver();
         driver.manage().timeouts().setScriptTimeout(20, TimeUnit.SECONDS);
     }
 
-  /*  @AfterClass
-    public static void closeDriver() {
-        Driver.getWebDriver().close();
-    }*/
+
 
 }
